87ba324180d7acdd2cb619375bf5726d2f9185ff
masterweed <marianopuchetta@hotmail.com>
2018-06-21 17:32:58 -0300

    add timer

--- tictac.js
+++ tictac.js
@@ -1,53 +1,53 @@
 var origBoard;
-const huPlayer='0';
-const aiPlayer='X';
+const huPlayer = '0';
+const aiPlayer = 'X';
 const winCombos = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [6,4,2]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ];
 const cells = document.querySelectorAll('.cell');
 
 startGame();
 
 function startGame() {
-  document.querySelector(".endgame").style.display="none";
-  origBoard=Array.from(Array(9).keys());
-  for (var i=0; i<cells.length; i++) {
-    cells[i].innerText='';
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
     cells[i].addEventListener('click', turnClick, false);
   }
 }
 
-function turnClick(square){
-  if (typeof origBoard[square.target.id]=='number') {
+function turnClick(square) {
+  if (typeof origBoard[square.target.id] == 'number') {
     turn(square.target.id, huPlayer);
     if (!checkTie()) turn(bestSpot(), aiPlayer);
   }
 }
 
-function turn (squareId, player) {
-  origBoard[squareId]=player;
+function turn(squareId, player) {
+  origBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
   let gameWon = checkWin(origBoard, player)
   if (gameWon) gameOver(gameWon)
 }
 
 function checkWin(board, player) {
-  let plays = board.reduce((a,e,i) =>
-      (e===player)? a.concat(i):a,[]);
-  let gameWon=null;
-  for(let[index, win] of winCombos.entries()) {
-        if (win.every(elem => plays.indexOf (elem) >-1)) {
-            gameWon = {index:index, player:player};
-            break;
-          }
+  let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
   }
   return gameWon;
 }
@@ -55,34 +55,81 @@ function checkWin(board, player) {
 function gameOver(gameWon) {
   for (let index of winCombos[gameWon.index]) {
     document.getElementById(index).style.backgroundColor =
-    gameWon.player== huPlayer ? "blue" : "red";
+      gameWon.player == huPlayer ? "blue" : "red";
   }
-  for (var i = 0; i<cells.length; i++) {
+  for (var i = 0; i < cells.length; i++) {
     cells[i].removeEventListener('click', turnClick, false);
   }
-  declareWinner(gameWon.player==huPlayer ? "You win!" : "You lose.");
+  declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+  stop_timer();
 }
 function declareWinner(who) {
-	document.querySelector(".endgame").style.display = "block";
-	document.querySelector(".endgame .text").innerText = who;
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = who;
+  stop_timer();
 }
 
 function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number');
+  return origBoard.filter(s => typeof s == 'number');
 }
 
 function bestSpot() {
-	return emptySquares()[0];
+  return emptySquares()[0];
 }
 
 function checkTie() {
-	if (emptySquares().length == 0) {
-		for (var i = 0; i < cells.length; i++) {
-			cells[i].style.backgroundColor = "green";
-			cells[i].removeEventListener('click', turnClick, false);
-		}
-		declareWinner("Tie Game!")
-		return true;
-	}
-	return false;
+  if (emptySquares().length == 0) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("Tie Game!")
+    stop_timer();
+    return true;
+  }
+  return false;
+}
+
+
+//TIMER
+window.onload = function () {
+
+  visor = document.getElementById("reloj"); //locate clock screen
+  //starts variables:
+  var motion = 0; //timer control
+  var cro = 0; //Initial status of the chronometer.
+
+
+}
+//Starts the chrono
+function start_time() {
+  emp = new Date() //Date at the start moment
+  elcrono = setInterval(time, 10); //timer function.
+  motion = 1 //we indicate that it has been launched.
+}
+//timer function			
+function time() {
+  actual = new Date(); //fecha a cada instante
+  //tiempo del crono (cro) = fecha instante (actual) - initial date (emp)
+  cro = actual - emp; //milliseconds elapsed.
+  cr = new Date(); // passed the num. from milliseconds to date object.
+  cr.setTime(cro);
+  //get the diferent formats of dates
+  cs = cr.getMilliseconds(); //milisegundos 
+  cs = cs / 10; //to hundredths of seconds.
+  cs = Math.round(cs); //Round the hundredths
+  sg = cr.getSeconds(); //seconds 
+  mn = cr.getMinutes(); //minutes
+
+  //Always puts 2 cipher	 
+  if (cs < 10) { cs = "0" + cs; }
+  if (sg < 10) { sg = "0" + sg; }
+  if (mn < 10) { mn = "0" + mn; }
+  //put the result on the visor		 
+  visor.innerHTML = sg + " : " + cs;
+}
+//Stops the clock
+function stop_timer() {
+  clearInterval(elcrono); //stops the crono
+  motion = 0; //
 }
